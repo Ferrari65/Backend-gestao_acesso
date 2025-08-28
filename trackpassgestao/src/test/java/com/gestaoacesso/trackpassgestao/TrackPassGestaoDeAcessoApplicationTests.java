ef549d27bada682539c88e82e6fb3f56209068be
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-28 11:18:13 -0300

    Inicialização

---
+++ trackpassgestao/src/test/java/com/gestaoacesso/trackpassgestao/TrackPassGestaoDeAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoacesso.trackpassgestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackPassGestaoDeAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
