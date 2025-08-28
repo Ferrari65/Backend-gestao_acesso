73ddbae12e9ea058187f299645bac3e2142d8c7c
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-08-28 10:55:40 -0300

    inicialização do projeto

---
+++ trackpassgestao/src/main/java/com/gestaoacesso/trackpassgestao/TrackPassGestaoDeAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoacesso.trackpassgestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackPassGestaoDeAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackPassGestaoDeAcessoApplication.class, args);
+	}
+
+}
