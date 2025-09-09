22102fbf4c0f61fd02e24780ec0daf9c83d71c35
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2025-09-08 21:38:17 -0300

    Atualizar o CorsConfig.java

--- src/main/java/com/infra/security/CorsConfig.java
+++ src/main/java/com/infra/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:8081"
+                                "http://localhost:5173"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
